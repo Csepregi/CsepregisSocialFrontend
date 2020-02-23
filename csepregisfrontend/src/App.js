ff0aa2a200ee0df513ccda3c523b1d8687579728
Csepregi <csepregigabor02@gmail.com>
2020-02-23 21:13:21 +0100

    authentication added

--- csepregisfrontend/src/App.js
+++ csepregisfrontend/src/App.js
@@ -3,6 +3,7 @@ import Place from './components/Place'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
 import placeService from './services/places'
+import loginService from './services/login'
 import { CardDeck } from 'react-bootstrap'
 
 const App = () => {
@@ -11,12 +12,47 @@ const App = () => {
   const [newDescription, setNewDescription] = useState('')
   const [newLocation, setNewLocation] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     placeService
       .getAll()
       .then(initialPlaces => setPlaces(initialPlaces))
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedPlaceappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      placeService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      window.localStorage.setItem(
+        'loggedPlaceappUser', JSON.stringify(user)
+      )
+
+      placeService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      setErrorMessage('Wrong Credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   //updating the placelist to render once blog has been deleted
   const deletePlace = (id, name) => {
@@ -63,47 +99,83 @@ const App = () => {
       })
   }
 
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type='text'
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type='password'
+          value={password}
+          name="password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const placeForm = () => (
+    <form onSubmit={addPlace}>
+      <div>
+        <label>
+          Name
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Description
+          <input
+            value={newDescription}
+            onChange={handleDescriptionChange}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Location
+          <input
+            value={newLocation}
+            onChange={handleLocationChange}
+          />
+        </label>
+      </div>
+      <button type="submit">save</button>
+    </form>
+  )
+
   return (
     <div>
       <h1>Shared places</h1>
 
       <Notification message={errorMessage} />
-
+      <h2>Login</h2>
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+          {placeForm()}
+        </div>
+      }
       <div>
       </div>
+      <h2>Locations</h2>
       <CardDeck>
         {rows()}
       </CardDeck>
-      <form onSubmit={addPlace}>
-        <div>
-          <label>
-            Name
-          <input
-              value={newName}
-              onChange={handleNameChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Description
-          <input
-              value={newDescription}
-              onChange={handleDescriptionChange}
-            />
-          </label>
-        </div>
-        <div>
-          <label>
-            Location
-          <input
-              value={newLocation}
-              onChange={handleLocationChange}
-            />
-          </label>
-        </div>
-        <button type="submit">save</button>
-      </form>
+
 
       <Footer />
     </div>
--- csepregisfrontend/src/services/places.js
+++ csepregisfrontend/src/services/places.js
@@ -1,21 +1,24 @@
 import axios from 'axios'
 const baseUrl = '/api/places'
 
-const getAll = () => {
-	const request = axios.get(baseUrl)
-	const nonExisting = {
-		id: 10000,
-		name: 'This note is not saved to server',
-		description: 'hello',
-		location: 'palermo',
-		date: '2019-05-30T17:30:31.098Z'
-	}
-	return request.then(response => response.data.concat(nonExisting))
+let token = null
+
+const setToken = newToken => {
+	token = `bearer ${newToken}`
 }
 
-const create = newObject => {
-	const request = axios.post(baseUrl, newObject)
+const getAll = () => {
+	const request = axios.get(baseUrl)
 	return request.then(response => response.data)
+}
+
+const create = async newObject => {
+	const config = {
+		headers: { Authorization: token },
+	}
+
+	const response = await axios.post(baseUrl, newObject, config)
+	return response.data
 }
 
 const update = (id, newObject) => {
@@ -25,8 +28,11 @@ const update = (id, newObject) => {
 
 const remove = async (id, name) => {
 	console.log(id);
-	const request = axios.delete(`${baseUrl}/${id}`);
+	const authoriz = {
+		headers: { Authorization: token },
+	}
+	const request = axios.delete(`${baseUrl}/${id}`, authoriz);
 	return request.then(response => response.data)
 }
 
-export default { getAll, create, update, remove }
+export default { getAll, create, update, remove, setToken }
